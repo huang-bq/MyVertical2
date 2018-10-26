4787097aa74caff16057dfd448a5e1a0c8b068cf
sophia huang <sophia@163.com>
2018-10-26 10:02:19 +0800

    add log

--- app/src/main/java/com/stone/verticalslide/MainActivity.java
+++ app/src/main/java/com/stone/verticalslide/MainActivity.java
@@ -2,11 +2,13 @@ package com.stone.verticalslide;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.Window;
 
 import com.stone.verticalslide.DragLayout.ShowNextPageNotifier;
 
 public class MainActivity extends FragmentActivity {
+	private static final  String rClassName = MainActivity.class.getSimpleName();
 
 	private VerticalFragment1 fragment1;
 	private VerticalFragment3 fragment3;
@@ -14,33 +16,42 @@ public class MainActivity extends FragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		if (null == savedInstanceState) {
+			Log.d(rClassName,"onCreate no saved previous state");
+		}else{
+			Log.d(rClassName,"onCreate restoring previous state");
+		}
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
 		initView();
-//		sddfs
-//		test tstdsfsfdhjf gj jlxxx alj
 	}
 
 	/**
 	 * 初始化View
 	 */
 	private void initView() {
-		fragment1 = new VerticalFragment1();
-		fragment3 = new VerticalFragment3();
+		try{
+			fragment1 = new VerticalFragment1();
+			fragment3 = new VerticalFragment3();
 
-		getSupportFragmentManager().beginTransaction()
-				.add(R.id.first, fragment1).add(R.id.second, fragment3)
-				.commit();
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.first, fragment1).add(R.id.second, fragment3)
+					.commit();
 
-		ShowNextPageNotifier nextIntf = new ShowNextPageNotifier() {
-			@Override
-			public void onDragNext() {
-				fragment3.initView();
-			}
-		};
-		draglayout = (DragLayout) findViewById(R.id.draglayout);
-		draglayout.setNextPageListener(nextIntf);
+			ShowNextPageNotifier nextIntf = new ShowNextPageNotifier() {
+				@Override
+				public void onDragNext() {
+					fragment3.initView();
+				}
+			};
+			draglayout = (DragLayout) findViewById(R.id.draglayout);
+			draglayout.setNextPageListener(nextIntf);
+
+		}catch (Exception e){
+			Log.d(rClassName,"initView()",e);
+		}
+
 	}
 
 }
